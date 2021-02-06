c4946e1cd0b03e1a241a2c72f463c7016c03182a
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-06 09:44:54 +0530

    commit

--- challenge-4/src/Merge.java
+++ challenge-4/src/Merge.java
@@ -3,7 +3,6 @@ public class Merge {
 public static void main(String[] args) {
 	int number1=10,number2=20;
 	System.out.println(number1+number2);
-	int data1=10,data2=20;
-	System.out.println(data1 + data2);
+	
 }
 }
